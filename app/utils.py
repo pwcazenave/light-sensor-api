7162f6f17df7e4def2ee5347991ec49ab62a01cc
Pierre Cazenave <pwcazenave@gmail.com>
2022-04-22 20:21:10 +0100

    Add the new class to gather the light intensity from the analog to digital converter (MCP3008).

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,7 @@
 import time
 
 import RPi.GPIO as GPIO
+from spidev import SpiDev
 
 
 def rc_time(pin_to_circuit, max_count=10000000):
@@ -28,3 +29,24 @@ def rc_time(pin_to_circuit, max_count=10000000):
     GPIO.cleanup()
 
     return count
+
+
+class MCP3008:
+    def __init__(self, bus=0, device=0):
+        self.bus, self.device = bus, device
+        self.spi = SpiDev()
+        self.open()
+        self.spi.max_speed_hz = 1000000  # 1MHz
+
+    def open(self):
+        self.spi.open(self.bus, self.device)
+        self.spi.max_speed_hz = 1000000  # 1MHz
+
+    def read(self, channel = 0):
+        adc = self.spi.xfer2([1, (8 + channel) << 4, 0])
+        data = ((adc[1] & 3) << 8) + adc[2]
+
+        return data
+
+    def close(self):
+        self.spi.close()
