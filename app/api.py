2df692fd09f50b4a3d3c6afe9428e688dc8206da
Pierre Cazenave <pwcazenave@gmail.com>
2022-04-22 20:20:09 +0100

    Fix the averaging.

--- app/api.py
+++ app/api.py
@@ -31,8 +31,8 @@ async def instantaneous():
     return flask.jsonify(intensity)
 
 
-@api.route('/last/<period>')
-async def averaged(period, strict_slashes=False):
+@api.route('/timed/last/<period>', strict_slashes=False)
+async def averaged(period):
     """ Average the light intensity over the given period """
 
     logger.info(f'Average light intensity over {period} seconds')
@@ -40,7 +40,9 @@ async def averaged(period, strict_slashes=False):
     start = datetime.now()
 
     cumulative_intensity = 0
+    count = 0
     while True:
+        count += 1
         now = datetime.now()
         if (now - start).total_seconds() < period:
             cumulative_intensity += rc_time(config.pin_to_circuit)
@@ -49,5 +51,6 @@ async def averaged(period, strict_slashes=False):
     finished = datetime.now()
     duration = (finished - start).total_seconds()
 
-    return flask.jsonify({'average_intensity': cumulative_intensity / duration, 'requested_seconds': period, 'actual_seconds': duration, 'time': datetime.now().isoformat()})
+    return flask.jsonify({'average_intensity': cumulative_intensity / count, 'requested_seconds': period, 'actual_seconds': duration, 'time': datetime.now().isoformat()})
+
 
