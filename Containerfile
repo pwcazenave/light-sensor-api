fe791ffd437f8e5c0697ee3a6ba4c74d9bbec23c
Pierre Cazenave <pwcazenave@gmail.com>
2022-04-22 21:53:33 +0100

    Tweak the podman run to mount the SPI device into the container.

--- Containerfile
+++ Containerfile
@@ -5,5 +5,5 @@ RUN pip3 install -r requirements.txt
 RUN mkdir $HOME/app
 COPY app/ $HOME/app
 COPY entrypoint.sh $HOME
-ENV PORT=8008
+ENV PORT=8000
 CMD ["./entrypoint.sh"]
--- make_image.sh
+++ make_image.sh
@@ -1,6 +1,10 @@
 podman stop light
 podman rm light
 podman build --tag light:latest .
-podman run -dt -p 8008:8008 --name light localhost/light:latest
+# --group-add keep-groups means we inherit groups from the user running podman
+# which means we can read /dev/spidev0.0. See:
+#    https://www.redhat.com/sysadmin/files-devices-podman
+# for details.
+podman run --detach --publish 8000:8000 --device /dev/spidev0.0:/dev/spidev0.0 --group-add keep-groups --name light localhost/light:latest
 sleep 1
 podman logs light
